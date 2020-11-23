ac013b2632eadb79f9ab5b6ba084efa17dbbd12f
Amadeusz Wieczorek <git@amadeusw.com>
2020-11-22 17:19:19 -0800

    get geolocation

--- src/script/app.ts
+++ src/script/app.ts
@@ -4,3 +4,31 @@ let heading = document.createElement('h1');
 heading.textContent = message;
 // add the heading the document
 document.body.appendChild(heading);
+
+function updateLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showLocation, showLocationError);
+    } else {
+        showLocationUnavailable();
+    }
+}
+
+function showLocation(position: GeolocationPosition) {
+    let locationContainer = document.getElementById('location') as HTMLElement;
+    if (locationContainer)
+        locationContainer.innerHTML = "Lat: " + position.coords.latitude + " Long: " + position.coords.longitude;
+}
+
+function showLocationError(error: GeolocationPositionError) {
+    let locationContainer = document.getElementById('location') as HTMLElement;
+    if (locationContainer)
+        locationContainer.innerHTML = error.message;
+}
+
+function showLocationUnavailable() {
+    let locationContainer = document.getElementById('location') as HTMLElement;
+    if (locationContainer)
+        locationContainer.innerHTML = 'Geolocation is not available';
+}
+
+updateLocation();
