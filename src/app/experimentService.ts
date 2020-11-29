9d376b8312ec9d3acf0983eab648fed4c2e277e0
Amadeusz Wieczorek <git@amadeusw.com>
2020-11-29 14:47:25 -0800

    use @most/adapter.createAdapter

--- src/app/experimentService.ts
+++ src/app/experimentService.ts
@@ -1,30 +1,36 @@
 import { combine, map, runEffects, startWith, tap } from '@most/core'
 import { mousemove } from '@most/dom-event'
 import { newDefaultScheduler } from '@most/scheduler'
+import { createAdapter } from '@most/adapter'
 
 export class ExperimentService {
     readonly out: HTMLElement;
-
+    inducer: (event: MouseEvent) => void;
     constructor() {
         this.out = document.getElementById('experiment') as HTMLElement;
         if (!this.out) {
             console.error("ExperimentService: output unavailable.");
         }
 
-        //document.addEventListener("mousemove", (event) => this.mouseMoveHandler(event));
+        let [induceFunction, inducedEvents] = createAdapter<MouseEvent>();
+        this.inducer = induceFunction;
+
+        document.addEventListener("mousemove", (event) => this.AddToMyStream(event));
 
         let double = (x:number, y:number) => x+y;
         let mouseEventToNumber = (e:MouseEvent) => e.clientX;
         let renderResult = (result:number) => { this.out.innerHTML = `${result}`; };
 
-        let mouseStream = mousemove(document, false);
-        let mouseX = startWith(0, map(mouseEventToNumber,  mouseStream));
+        //let mouseStream = mousemove(document, false);
+        //let mouseX = startWith(0, map(mouseEventToNumber,  mouseStream));
+        let mouseX = startWith(0, map(mouseEventToNumber, inducedEvents));
         let result = combine(double, mouseX, mouseX);
         let outputEffects = tap(renderResult, result);
         runEffects(outputEffects, newDefaultScheduler());
     }
 
-    mouseMoveHandler(event: MouseEvent): void {
-        this.out.innerHTML = `${event.clientX}`;
+    AddToMyStream(event: MouseEvent): void {
+        this.inducer(event);
+        //this.out.innerHTML = `${event.clientX}`;
     }
 }
